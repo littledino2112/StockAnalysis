ea09eeccb30153b895cffe35812f59ac3c48351c
Hoang Nguyen <huahoang2112@gmail.com>
2017-05-01 21:21:27 +0700

    Add function to query stock

---
+++ query_stock.m
@@ -0,0 +1,24 @@
+function [ valid, selected_stock ] = query_stock( handles )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    stocklist = get(handles.StockSelectionMenu,'String');
+    idx = get(handles.StockSelectionMenu,'Value');
+    selectedStock = stocklist{idx};
+    duration = get(handles.DurationEdit,'String');
+    duration = str2double(duration);
+    db_conn = handles.DatabaseConn;
+    start_date = num2str(floor(now) - duration);
+    sql_query = ['SELECT * FROM STOCK WHERE SYMBOL LIKE ''' selectedStock ''' AND DATE > ' start_date];
+    symbol_data = fetch(db_conn, sql_query);
+    if isempty(symbol_data)
+        valid = false;
+        % Pop up error message here
+        msgbox('No data');
+    else
+        valid = true;
+        selected_stock = fints(symbol_data.DATE, [symbol_data.OPEN symbol_data.HIGH symbol_data.LOW symbol_data.CLOSE symbol_data.VOLUME],...
+                        {'OPEN', 'HIGH', 'LOW', 'CLOSE', 'VOLUME'});
+    end
+
+end
+
