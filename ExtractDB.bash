9b6c227ea11d1fbf9a1927005c0429104e1d80d5
Hoang Nguyen <huahoang2112@gmail.com>
2017-05-10 07:03:07 +0700

    Export data under csv instead of sql format

--- ExtractDB.bash
+++ ExtractDB.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
-sqlite3 StockDB.db .dump > data.sql
 sqlite3 StockDB.db .schema > schema.sql
+sqlite3 -header -csv StockDB.db "select * from HOSE_STOCK_DIFF;" > hose_stock_diff_table.csv
+sqlite3 -header -csv StockDB.db "select * from STOCK;" > hose_stock_table.csv
+
 
