e7316d7fdedcb9b19a7d1f487e7cc5697fb1055a
Hoang Nguyen <huahoang2112@gmail.com>
2017-05-11 16:34:58 +0700

    Remove Data Load button callback. Database is connected before GUI is brought to visibility.

--- StockAnalysis.m
+++ StockAnalysis.m
@@ -95,33 +95,6 @@ function varargout = StockAnalysis_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in LoadDataButton.
-function LoadDataButton_Callback(hObject, eventdata, handles)
-% hObject    handle to LoadDataButton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-    load('config.mat');
-    path_to_database = path_to_db;
-    handles.DatabaseConn = database(path_to_database,'','','org.sqlite.JDBC',strcat('jdbc:sqlite:',path_to_database));
-    db_conn = handles.DatabaseConn;
-    if isempty(db_conn.Message)
-        s = setdbprefs;
-        s.DataReturnFormat = 'table';
-        setdbprefs(s);
-        set(handles.LoadDataStatus,'String','Connected to Database');
-        % Populate Stock Selection Dropdown list
-        sql_query = 'SELECT DISTINCT SYMBOL FROM STOCK';
-        data = fetch(db_conn, sql_query);
-        set(handles.StockSelectionMenu,'String',data.SYMBOL);
-    else
-        set(handles.LoadDataStatus,'String','Error connect to Database');
-    end
-    guidata(hObject, handles);
-
-
-
-
-
 % --- Executes on mouse press over figure background.
 function MainFigure_ButtonDownFcn(hObject, eventdata, handles)
 % hObject    handle to MainFigure (see GCBO)
@@ -157,9 +130,9 @@ function StockSelectionMenu_Callback(hObject, eventdata, handles)
 
 % Hints: contents = cellstr(get(hObject,'String')) returns StockSelectionMenu contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from StockSelectionMenu
-   [valid, selected_stock] = query_stock(handles);
-    handles.SelectedStock = selected_stock;
-    guidata(hObject, handles);
+[valid, selected_stock] = query_stock(handles);
+handles.SelectedStock = selected_stock;
+guidata(hObject, handles);
 
 % --- Executes during object creation, after setting all properties.
 function StockSelectionMenu_CreateFcn(hObject, eventdata, handles)
@@ -181,9 +154,9 @@ function DurationEdit_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of DurationEdit as text
 %        str2double(get(hObject,'String')) returns contents of DurationEdit as a double
-    [valid, selected_stock] = query_stock(handles);
-    handles.SelectedStock = selected_stock;
-    guidata(hObject, handles);
+[valid, selected_stock] = query_stock(handles);
+handles.SelectedStock = selected_stock;
+guidata(hObject, handles);
 
     
 % --- Executes during object creation, after setting all properties.
@@ -201,23 +174,23 @@ end
 
 % --- Executes on button press in OBVChartButton.
 function OBVChartButton_Callback(hObject, eventdata, handles)
-    % hObject    handle to OBVChartButton (see GCBO)
-    % eventdata  reserved - to be defined in a future version of MATLAB
-    % handles    structure with handles and user data (see GUIDATA)
-    if ~isempty(handles.SelectedStock)
-        figure('Name','On Balance Volume');
-        subplot(3,1,1);
-        candle(handles.SelectedStock);
-        subplot(3,1,2);   
-        plot(onbalvol(handles.SelectedStock));
-        % Plot volume in bar graph
-        ax = subplot(3,1,3);
-        data_extract = fts2mat(handles.SelectedStock.VOLUME,1);
-        bar(data_extract(:,1),data_extract(:,2));
-        ax.XTick = [data_extract(:,1); data_extract(end,1) + 1];
-        datetick(ax,'x','dd-mmm-yy','keepticks');
-        xtickangle(ax,90);
-    end
+% hObject    handle to OBVChartButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+if ~isempty(handles.SelectedStock)
+    figure('Name','On Balance Volume');
+    subplot(3,1,1);
+    candle(handles.SelectedStock);
+    subplot(3,1,2);   
+    plot(onbalvol(handles.SelectedStock));
+    % Plot volume in bar graph
+    ax = subplot(3,1,3);
+    data_extract = fts2mat(handles.SelectedStock.VOLUME,1);
+    bar(data_extract(:,1),data_extract(:,2));
+    ax.XTick = [data_extract(:,1); data_extract(end,1) + 1];
+    datetick(ax,'x','dd-mmm-yy','keepticks');
+    xtickangle(ax,90);
+end
 
 
 % --- Executes on button press in OverviewButton.
@@ -232,10 +205,10 @@ function CandleChartButton_Callback(hObject, eventdata, handles)
 % hObject    handle to CandleChartButton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    if ~isempty(handles.SelectedStock)
-        figure('Name','Candle Chart');
-        candle(handles.SelectedStock);
-    end
+if ~isempty(handles.SelectedStock)
+    figure('Name','Candle Chart');
+    candle(handles.SelectedStock);
+end
 
 
 % --- Executes on button press in CloseAllButton.
@@ -438,27 +411,27 @@ function FilterButton_Callback(hObject, eventdata, handles)
 % Flows:
 %   - Check which condition is checked
 %   - 
-    date_start = handles.FilterDateRangeEdit.String;
-    date_start = floor(now) - str2double(date_start);
-    stock_diff_table = handles.Database.TableNames.HOSE_STOCK_DIFF;
-    stock_table = handles.Database.TableNames.STOCK;
-    if handles.FilterPriceChangeCheck.Value
-       price_change_in_percentage = handles.FilterPriceChangeEdit.String;
-       sql_query = ['SELECT ' stock_diff_table '.SYMBOL, SUM(CLOSE_DIFF_PERCENTAGE) AS SUM_PRICE_CHANGE, AVG(VOLUME) AS AVG_VOLUME, MAX(CLOSE) AS MAX_CLOSE '...
-                    'FROM ' stock_diff_table ' INNER JOIN ' stock_table ' '...
-                    'ON ' stock_table '.SYMBOL_DATE = ' stock_diff_table '.SYMBOL_DATE '...
-                    'WHERE ' stock_diff_table '.DATE > ' num2str(date_start) ' '...
-                    'GROUP BY ' stock_diff_table '.SYMBOL '...
-                    'HAVING SUM_PRICE_CHANGE > ' price_change_in_percentage ' '...
-                    'ORDER BY SUM_PRICE_CHANGE DESC'];
-       symbol_list = fetch(handles.DatabaseConn, sql_query);
-       if ~(isempty(symbol_list))
-           symbol_list = table2cell(symbol_list);
-           col_names = {'Symbol','Sum Price Change','Average Volume','Max Close'};
-           handles.FilterStockTable.ColumnName = col_names;
-           handles.FilterStockTable.Data = symbol_list; 
-       end  
-    end
+date_start = handles.FilterDateRangeEdit.String;
+date_start = floor(now) - str2double(date_start);
+stock_diff_table = handles.Database.TableNames.HOSE_STOCK_DIFF;
+stock_table = handles.Database.TableNames.STOCK;
+if handles.FilterPriceChangeCheck.Value
+   price_change_in_percentage = handles.FilterPriceChangeEdit.String;
+   sql_query = ['SELECT ' stock_diff_table '.SYMBOL, SUM(CLOSE_DIFF_PERCENTAGE) AS SUM_PRICE_CHANGE, AVG(VOLUME) AS AVG_VOLUME, MAX(CLOSE) AS MAX_CLOSE '...
+                'FROM ' stock_diff_table ' INNER JOIN ' stock_table ' '...
+                'ON ' stock_table '.SYMBOL_DATE = ' stock_diff_table '.SYMBOL_DATE '...
+                'WHERE ' stock_diff_table '.DATE > ' num2str(date_start) ' '...
+                'GROUP BY ' stock_diff_table '.SYMBOL '...
+                'HAVING SUM_PRICE_CHANGE > ' price_change_in_percentage ' '...
+                'ORDER BY SUM_PRICE_CHANGE DESC'];
+   symbol_list = fetch(handles.DatabaseConn, sql_query);
+   if ~(isempty(symbol_list))
+       symbol_list = table2cell(symbol_list);
+       col_names = {'Symbol','Sum Price Change','Average Volume','Max Close'};
+       handles.FilterStockTable.ColumnName = col_names;
+       handles.FilterStockTable.Data = symbol_list; 
+   end  
+end
 
 
 function FilterDateRangeEdit_Callback(hObject, eventdata, handles)
@@ -488,19 +461,19 @@ function MarketTrendButton_Callback(hObject, eventdata, handles)
 % hObject    handle to MarketTrendButton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    duration = handles.DurationEdit.String;
-    duration = str2double(duration);
-    [dates, results] = compute_market_trend(handles.DatabaseConn,duration);
-    figure('Name','Market trend');
-    subplot(2,1,1);
-    if ~isempty(handles.SelectedStock)
-        candle(handles.SelectedStock);
-    end
-    ax = subplot(2,1,2);
-    plot(ax,dates,results);
-    ax.XTick = linspace(dates(1),dates(end)+1,4);
-    datetick(ax,'x','dd-mmm-yy','keepticks');
-    xtickangle(ax,90);
+duration = handles.DurationEdit.String;
+duration = str2double(duration);
+[dates, results] = compute_market_trend(handles.DatabaseConn,duration);
+figure('Name','Market trend');
+subplot(2,1,1);
+if ~isempty(handles.SelectedStock)
+    candle(handles.SelectedStock);
+end
+ax = subplot(2,1,2);
+plot(ax,dates,results);
+ax.XTick = linspace(dates(1),dates(end)+1,4);
+datetick(ax,'x','dd-mmm-yy','keepticks');
+xtickangle(ax,90);
 
 
 % --- Executes on button press in pushbutton9.
@@ -516,9 +489,9 @@ function FilterStockTable_CellSelectionCallback(hObject, eventdata, handles)
 % eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
 %	Indices: row and column indices of the cell(s) currently selecteds
 % handles    structure with handles and user data (see GUIDATA)
-    selected_item = hObject.Data(eventdata.Indices(1),eventdata.Indices(2));
-    idx = find(strcmp(handles.StockSelectionMenu.String,selected_item));
-    if ~(isempty(idx))
-       handles.StockSelectionMenu.Value = idx;
-       StockSelectionMenu_Callback(handles.StockSelectionMenu, eventdata, handles); 
-    end
+selected_item = hObject.Data(eventdata.Indices(1),eventdata.Indices(2));
+idx = find(strcmp(handles.StockSelectionMenu.String,selected_item));
+if ~(isempty(idx))
+   handles.StockSelectionMenu.Value = idx;
+   StockSelectionMenu_Callback(handles.StockSelectionMenu, eventdata, handles); 
+end
