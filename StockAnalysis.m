7095d10792ce4343d0a4f8570483463525d63836
Hoang Nguyen <huahoang2112@gmail.com>
2017-05-12 11:56:47 +0700

    Add simple moving average of 5 days to AD line

--- StockAnalysis.m
+++ StockAnalysis.m
@@ -476,16 +476,19 @@ function MarketTrendButton_Callback(hObject, eventdata, handles)
 duration = handles.DurationEdit.String;
 duration = str2double(duration);
 [dates, results] = compute_market_trend(handles.DatabaseConn,duration);
+% Compute simple moving average on 5-day basis
+sma = tsmovavg(results,'s',5,2); % 2 indicates the input vector is row-oriented matrix where each column is an observation
 figure('Name',['Market trend - ' handles.SelectedStock.Name]);
 subplot(2,1,1);
 if ~isempty(handles.SelectedStock.TimeSeriesObj)
     candle(handles.SelectedStock.TimeSeriesObj);
 end
 ax = subplot(2,1,2);
-plot(ax,dates,results);
+plot(ax,dates,results,dates,sma);
 title('Cumulative Advance-Decline line');
 ax.XTick = linspace(dates(1),dates(end)+1,4);
 datetick(ax,'x','dd-mmm-yy','keepticks');
+legend('Raw data','SMA5');
 xtickangle(ax,90);
 
 
