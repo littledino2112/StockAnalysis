ce6da760ed10e17480387961e96649d6d798fd94
Hoang Nguyen <huahoang2112@gmail.com>
2017-05-15 21:47:08 +0800

    Add extracted csv for 2 new tables. Modify export data bash file. Update 2 original stock tables.

--- ExtractDB.bash
+++ ExtractDB.bash
@@ -2,5 +2,7 @@
 sqlite3 StockDB.db .schema > schema.sql
 sqlite3 -header -csv StockDB.db "select * from HOSE_STOCK_DIFF;" > hose_stock_diff_table.csv
 sqlite3 -header -csv StockDB.db "select * from STOCK;" > hose_stock_table.csv
+sqlite3 -header -csv StockDB.db "select * from HOSE_SELL_BUY_NATIONAL;" > hose_stock_sell_buy_national_table.csv
+sqlite3 -header -csv StockDB.db "select * from HOSE_SELL_BUY_FOREIGNER;" > hose_stock_sell_buy_foreigner_table.csv
 
 
